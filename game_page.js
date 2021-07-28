f4f8f31e2b726a16fc3eb8c4a4da34bcf86cb41c
varnikap <78656240+varnikap@users.noreply.github.com>
2021-07-28 16:46:32 -0400

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -6,13 +6,14 @@ player2_score=0;
 
 document.getElementById("player1_name").innerHTML = player1_name +" :";
 document.getElementById("player2_name").innerHTML = player2_name +" :";
+
 document.getElementById("player1_score").innerHTML = player1_score ;
 document.getElementById("player2_score").innerHTML = player2_score ;
 
 document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
 document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
 
-console.log(player2_namev);
+
 
 
 function send(){
@@ -21,7 +22,7 @@ number2 = document.getElementById("number2").value;
 actual_answer = parseInt(number1) * parseInt(number2);
 
 question_number = "<h4>" + number1 + "X" + number2 +"</h4>";
-input_box = "<br> Answer: <input type= 'text' id=' input_check_box'>";
+input_box = "<br> Answer: <input type= 'text' id='input_check_box' ";
 check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
 row = question_number + input_box + check_button ;
 document.getElementById("output").innerHTML = row;
@@ -29,6 +30,10 @@ document.getElementById("output").innerHTML = row;
 document.getElementById("number1").value = "";
 document.getElementById("number2").value = "";
 }
+
+question_turn = "player1";
+answer_turn = "player2";
+
 function check()
 {
 get_answer = document.getElementById("input_check_box").value;
@@ -38,13 +43,13 @@ if(get_answer == actual_answer)
 {
     if(answer_turn == "player1")
     {
-        player1_score = player1_score + 1;
-        document.getElementById("player1_score").innerHTML = player1_score;
+        update_player1_score = player1_score + 1;
+        document.getElementById("player1_score").innerHTML = update_player1_score;
     }
     else
     {
-        player2_score = player2_score + 1;
-        document.getElementById("player2_score").innerHTML = player2_score;
+        update_player2_score = player2_score + 1;
+        document.getElementById("player2_score").innerHTML = update_player2_score;
     }
 }
 if(question_turn =="player1")
